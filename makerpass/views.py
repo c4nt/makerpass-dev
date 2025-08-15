6b8fd58a63064fa1cf11c060ddb9ce10375d3b29
Marcos <marcosolivee136@gmail.com>
2025-08-15 16:18:04 -0300

    adiciona ajustes no código da view para melhor legibilidade

--- makerpass/views.py
+++ makerpass/views.py
@@ -28,13 +28,13 @@ class PaginaRegistroPontoView(View):
         if not matricula:
             messages.error(request, "Matrícula não informada.")
             return redirect('pagina_registro_ponto')
-
         try:
             servidor = Servidor.objects.get(matricula=matricula)
         except Servidor.DoesNotExist:
             messages.error(request, "Bolsista não encontrado.")
             return redirect('pagina_registro_ponto')
-
+        
+        #--- INTEVALO DE 1 MIN. PARA REGISTRO DE PONTO ---
         ultimo_ponto = Ponto.objects.filter(bolsista=servidor).last()
         agora = timezone.now()
 
@@ -45,6 +45,8 @@ class PaginaRegistroPontoView(View):
                 messages.error(request, f"Aguarde {segundos_restantes} segundos para registrar um novo ponto.")
                 return redirect('pagina_registro_ponto')
 
+
+        #--- LÓGICA PARA DELETAR PONTOS PENDENTES DO DIA ANTERIOR ---
         eh_entrada = not ultimo_ponto.eh_entrada if ultimo_ponto else True
         
         if not eh_entrada:
@@ -54,7 +56,24 @@ class PaginaRegistroPontoView(View):
 
         ponto_criado = Ponto.objects.create(bolsista=servidor, eh_entrada=eh_entrada)
 
-        # --- LÓGICA DE SUCESSO COM SESSÃO ---
+        #--- ENVIA DADOS PARA A PÁGINA DE SUCESSO ATRAVÉS DA SESSAO ---
+        sucesso_ponto(request, ponto_criado, servidor)
+        
+        return redirect('pagina_sucesso_ponto')
+
+class PaginaSucessoPontoView(TemplateView):
+    template_name = 'makerpass/sucesso_ponto.html'
+    
+    def get_context_data(self, **kwargs):
+        ultimo_ponto = Ponto.objects.all().last()
+        
+        context = super().get_context_data(**kwargs)
+        context['ultimo_ponto'] = ultimo_ponto
+        context['horas_trabalhadas_dia'] = self.request.session.pop('horas_trabalhadas_dia', None)
+        
+        return context
+
+def sucesso_ponto(request, ponto_criado, servidor):
         # Limpa dados antigos da sessão para garantir consistência
         request.session.pop('horas_trabalhadas_dia', None)
 
@@ -70,6 +89,7 @@ class PaginaRegistroPontoView(View):
 
             total_duration = timedelta()
             entrada_time = None
+
             for ponto in pontos_do_dia:
                 if ponto.eh_entrada:
                     entrada_time = ponto.data_hora_do_ponto
@@ -84,135 +104,3 @@ class PaginaRegistroPontoView(View):
             
             # Armazena os dados na sessão para a próxima página
             request.session['horas_trabalhadas_dia'] = horas_trabalhadas_str
-        
-        # --- FIM DA LÓGICA DE SUCESSO COM SESSÃO ---  
-
-        ordem_para_arduino = "ABRIR" if eh_entrada else None
-        
-        return redirect('pagina_sucesso_ponto')
-
-class PaginaSucessoPontoView(TemplateView):
-    template_name = 'makerpass/sucesso_ponto.html'
-    
-    def get_context_data(self, **kwargs):
-        ultimo_ponto = Ponto.objects.all().last()
-        
-        context = super().get_context_data(**kwargs)
-        context['ultimo_ponto'] = ultimo_ponto
-        context['horas_trabalhadas_dia'] = self.request.session.pop('horas_trabalhadas_dia', None)
-        
-        return context
-    
-# @method_decorator(csrf_exempt, name='dispatch')
-# class ApiRegistrarPontoView(View):
-
-#     async def post(self, request, *args, **kwargs):
-#         try:
-#             data = json.loads(request.body)
-#             id_sensor = data['id_sensor']
-#             print(id)
-
-#             # A lógica de negócio principal é exatamente a mesma.
-#             servidor = await Servidor.objects.aget(id_sensor_biometrico=id_sensor)
-#             ultimo_ponto = await Ponto.objects.filter(bolsista=servidor).alast()
-
-#             if ultimo_ponto:
-#                 agora = timezone.now()
-#                 tempo_desde_ultimo_ponto = agora - ultimo_ponto.data_hora_do_ponto
-
-#                 if tempo_desde_ultimo_ponto < timedelta(minutes=1):
-#                     segundos_restantes = int(60 - tempo_desde_ultimo_ponto.total_seconds())
-#                     print(f"[API] Registro bloqueado para {servidor.matricula}. Tolerância de 1 min. Faltam {segundos_restantes}s.")
-                    
-#                     return JsonResponse({
-#                         "status": "error",
-#                         "message": f"Aguarde {segundos_restantes} segundos para registrar um novo ponto."
-#                     }, status=429)
-
-#             eh_entrada = not ultimo_ponto.eh_entrada if ultimo_ponto else True
-
-#             ponto_criado = await Ponto.objects.acreate(bolsista=servidor, eh_entrada=eh_entrada)
-#             print(ponto_criado)
-
-#             ordem_para_arduino = "ABRIR" if eh_entrada else None
-
-            
-#             # TODO: Notificar o stream de eventos sobre o 'ponto_criado'.
-#             # (Nosso próximo passo!)
-
-#             return JsonResponse({
-#                 "status": "success",
-#                 "message": "Ponto registrado com sucesso.",
-#                 "servidor": servidor.matricula,
-#                 "tipo_registro": "Entrada" if eh_entrada else "Saída",
-#                 "ordem":ordem_para_arduino
-#             })
-#         except Servidor.DoesNotExist:
-#             return JsonResponse({"status": "error", "message": "Matrícula não encontrada."}, status=404)
-#         except Exception as e:
-#             return JsonResponse({"status": "error", "message": str(e)}, status=500)
-
-#     async def get(self, request, *args, **kwargs):
-#         # É uma boa prática responder a métodos não permitidos.
-#         return JsonResponse({"status": "error", "message": "Método GET não permitido."}, status=405)
-
-# class PontoStreamEventsView(View):
-#     """
-#     Esta classe lida com a conexão de Streaming para o frontend.
-#     A lógica agora está dentro do método 'get'.
-#     """
-#     async def get(self, request, *args, **kwargs):
-#         async def event_stream():
-#             # A lógica de notificação virá aqui.
-#             # Por enquanto, a view está pronta para recebê-la.
-#             while True:
-#                 # TODO: Implementar a lógica para esperar por uma notificação
-#                 # da ApiRegistrarPontoView.
-#                 await asyncio.sleep(1)
-
-#         return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
-
-
-# class RegistroPonto(View):
-    
-#     async def post(self, request, *args, **kwargs):
-        
-#         print(request.POST['matricula'])
-#         bolsista = Servidor.objects.get(matricula=request.POST['matricula'])
-#         prox_tipo_eh_entrada = True
-
-#         if Ponto.objects.filter(bolsista=bolsista):
-#             if Ponto.objects.filter(bolsista=bolsista).latest('data_hora_do_ponto').eh_entrada == True:
-#                 prox_tipo_eh_entrada = False
-#             else:
-#                 prox_tipo_eh_entrada = True
-        
-
-#         novo_ponto = Ponto.objects.create(
-#             bolsista=bolsista,
-#             eh_entrada=prox_tipo_eh_entrada
-#         )
-
-#         hora_local = timezone.localtime(novo_ponto.data_hora_do_ponto)
-#         hora_formatada = hora_local.strftime('%H:%M')
-
-
-#         tipo_acao = "Entrada" if novo_ponto.eh_entrada else "Saída"
-#         messages.success(request, f"{tipo_acao} registrada com sucesso ás {hora_formatada}!")
-
-#         return redirect('registrar_ponto')
-
-#     async def get(self, request, *args, **kwargs):
-#         async def event_stream():
-#             async for novo_dado in monitora_arquivo_json():
-#                 formatted_data = f"data: {json.dumps(novo_dado)}\n\n"
-#                 yield formatted_data.encode('utf-8')
-        
-#         response = StreamingHttpResponse(event_stream(), content_type="text/event-stream")
-#         response['Cache-Control'] = 'no-cache'
-#         return response
-
-
-#         return render(request, 'makerpass/registrar_ponto.html')
-
-
