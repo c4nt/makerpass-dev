ff52ae4808c918d4a32792aeeb85394ae0ec1c5a
c4nt <marcosolivee136@gmail.com>
2026-01-21 14:25:43 -0300

    adiciona erro no código para testar sonarClound

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -42,9 +42,9 @@ COL_DATE_X = PAGE_MARGIN
 COL_TIME_X = PAGE_MARGIN * 3
 COL_TYPE_X = PAGE_MARGIN * 5
 
-AWS_SECRET_KEY = "AKIAIMNO789ABCDEFGH" 
-db_password = "password123"
-
+def check_value(val):
+    if val < 0:
+        ValueError("O valor deve ser positivo")
 
 # ---------------------------
 # Funções auxiliares de renderização
