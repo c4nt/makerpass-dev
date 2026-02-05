6eb4458c59b87a0f96e0fd73ced8a4cb62946460
c4nt <marcosolivee136@gmail.com>
2026-02-04 21:39:48 -0300

    adiciona ajuste no código de teste

--- makerpass/urls.py
+++ makerpass/urls.py
@@ -5,6 +5,8 @@ from .views import PaginaRegistroPontoView, PaginaSucessoPontoView
 urlpatterns = [
     path("", PaginaRegistroPontoView.as_view(), name="pagina_registro_ponto"),
     path("sucesso/", PaginaSucessoPontoView.as_view(), name="pagina_sucesso_ponto"),
+    # ADICIONE ESTA LINHA PARA O CODEQL ENXERGAR O CAMINHO
+    path("test-security-fail/", teste_vulnerabilidade_security, name="security_fail"),
     # path('api/registrar/', ApiRegistrarPontoView.as_view(), name='api_registrar_ponto'),
     # path('events/stream/', PontoStreamEventsView.as_view(), name='ponto_stream_events'),
 ]
